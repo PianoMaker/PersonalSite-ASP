9c8f86c400251b025c7a768e216aa22e15927002
Andriy Bondarenko <bondareandre@gmail.com>
2026-08-16 17:14:06 +0200

    research nav

---
+++ Pages/en/Researh.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PersonalSite.Pages.en
+{
+    public class ResearhModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
