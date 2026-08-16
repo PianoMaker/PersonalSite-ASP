25ad159aafc44c91d0edc1b52f4ce0b53b4c940e
Andriy Bondarenko <bondareandre@gmail.com>
2026-08-16 23:54:59 +0200

    restore Music.cshtml

---
+++ Pages/uk/Music.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using PersonalSite.Data;
+
+namespace PersonalSite.Pages.uk
+{
+    public class MusicModel : PageModel
+    {
+        public List<MusicalWork> MusicalWorks { get; set; } = new();
+
+        public void OnGet()
+        {
+            MusicalWorks = MusicData.Works;
+        }
+    }
+}
