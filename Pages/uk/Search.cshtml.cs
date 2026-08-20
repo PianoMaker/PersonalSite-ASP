c7fba469264d7aee862d901c70e91d0e0073a185
Andriy Bondarenko <bondareandre@gmail.com>
2026-08-20 17:48:52 +0300

    Search fix and design

--- Pages/uk/Search.cshtml.cs
+++ Pages/uk/Search.cshtml.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using PersonalSite.Data;
+using System.IO;
 
 namespace PersonalSite.Pages.uk
 {
-
 
     public class Results
     {
@@ -15,11 +15,32 @@ namespace PersonalSite.Pages.uk
         public List<WorkLink> Links { get; set; } = new();
     }
 
+
+
+
     public class SearchModel : PageModel
     {
         public List<Results> Results { get; set; } = new();
 
+        private readonly ILogger<SearchModel> _logger;
+        private readonly IWebHostEnvironment _environment;
+
+        public SearchModel(IWebHostEnvironment environment, ILogger<SearchModel> logger)
+        {        
+            _logger = logger;
+            _environment = environment;
+        }
+
         public void OnGet(string? search)
+        {
+            
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                Results = Find(search);
+            }
+        }
+
+        public void OnPost(string? search)
         {
             if (!string.IsNullOrWhiteSpace(search))
             {
@@ -33,7 +54,21 @@ namespace PersonalSite.Pages.uk
 
             search = search.Trim();
 
-            foreach (var work in Data.Data.Works)
+            var path = Path.Combine(
+                _environment.ContentRootPath,
+                "Data",
+                "works.json"
+            );
+
+            var allWorks = Data.Data.GetDataFromJson(path);
+
+
+            _logger.LogInformation(
+                "Початок пошуку '{Search}' серед {Count} записів",
+                search,
+                allWorks.Count);
+
+            foreach (var work in allWorks)
             {
                 bool found =
                     work.UkTitle.Contains(search, StringComparison.OrdinalIgnoreCase)
