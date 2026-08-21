64c94060c3123d366d2f06c6e4eea53f77679977
Andriy Bondarenko <bondareandre@gmail.com>
2026-08-22 00:30:00 +0300

    Web.cshtml added

---
+++ Pages/uk/Web.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PersonalSite.Pages.uk
+{
+    public class WebModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
