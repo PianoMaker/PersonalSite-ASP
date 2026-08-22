ec8df90a925c38325ab0d27f2704885d6d46dca5
Andriy Bondarenko <bondareandre@gmail.com>
2026-08-22 20:13:09 +0300

    Web english version

---
+++ Pages/en/Web.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PersonalSite.Pages.en
+{
+    public class WebModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
